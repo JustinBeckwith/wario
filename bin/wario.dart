5b55dad7fa9c4752b860395ae2d2709b5f069a4d
Justin Beckwith <justin.beckwith@gmail.com>
2018-06-22 23:01:19 -0700

    get the right list of repos

--- bin/wario.dart
+++ bin/wario.dart
@@ -1,16 +1,17 @@
 import 'dart:io';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:json_object/json_object.dart';
 
 void main(List<String> arguments) {
   if (arguments.length == 0) {
     showUsage();
   }
   print(arguments);
-  var command = arguments[0];
+  final command = arguments[0];
+  final language = arguments[2];
   switch(command) {
     case 'init':
-      init();
+      init(language);
       break;
   }
 }
@@ -24,12 +25,16 @@ void showUsage() {
   exit(-1);
 }
 
-init() async {
+init(String language) async {
   const url = 'https://raw.githubusercontent.com/JustinBeckwith/sloth/master/repos.json';
-  var res = await http.get(url);
+  final res = await http.get(url);
   print('status: ${res.statusCode}');
-  RepoResponse data = JSON.decode(res.body);
-  print(data);
+  final RepoResponse data = new JsonObject.fromJsonString(res.body);
+  for(var repo in data.repos) {
+    if (repo.language == language) {
+      print(repo.repo);
+    }
+  }
 }
 
 abstract class RepoResponse {
