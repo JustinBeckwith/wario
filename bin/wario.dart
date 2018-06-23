61171d4822f7948ce899fcb525185ef843bfecde
Justin Beckwith <justin.beckwith@gmail.com>
2018-06-22 22:29:07 -0700

    do the first fetch

--- bin/wario.dart
+++ bin/wario.dart
@@ -1,7 +1,42 @@
 import 'dart:io';
-import 'package:args/args.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  print('hello world  🎉');
+  if (arguments.length == 0) {
+    showUsage();
+  }
   print(arguments);
+  var command = arguments[0];
+  switch(command) {
+    case 'init':
+      init();
+      break;
+  }
+}
+
+void showUsage() {
+  print(
+'''\$ wario
+  - init   Clone all repositories
+  - reset  Fetch all upstreams, and perform a hard reset
+''');
+  exit(-1);
+}
+
+init() async {
+  const url = 'https://raw.githubusercontent.com/JustinBeckwith/sloth/master/repos.json';
+  var res = await http.get(url);
+  print('status: ${res.statusCode}');
+  RepoResponse data = JSON.decode(res.body);
+  print(data);
+}
+
+abstract class RepoResponse {
+  List<Repo> repos;
+}
+
+abstract class Repo {
+  String repo;
+  String language;
 }
--- main.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:args/args.dart';
-
-void main(List<String> arguments) {
-  print('hello world  🎉');
-  print(arguments);
-}
