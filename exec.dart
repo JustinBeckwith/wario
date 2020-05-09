3cc74563d0dd5cd214dc3a7aedc0d35c87d89d31
Justin Beckwith <beckwith@google.com>
2020-05-08 23:15:17 -0700

    fix: iterate sync over exec command

--- exec.dart
+++ exec.dart
@@ -2,12 +2,13 @@ import 'dart:io';
 
 exec(List<String> params) async {
   final rootDir = new Directory('.wario');
-  rootDir.list().listen((entity) async {
+  final dirStream = rootDir.list();
+  await for (var entity in dirStream) {
     final stat = await entity.stat();
     if (stat.type == FileSystemEntityType.directory) {
       var results = await Process.run(params[0], params.sublist(1), workingDirectory: entity.path);
       stdout.write(results.stdout);
       stderr.write(results.stderr);
     }
-  });
+  }
 }
